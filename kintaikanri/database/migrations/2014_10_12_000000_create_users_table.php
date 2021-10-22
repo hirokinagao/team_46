1565d62dacac1ba2cec7bec2d6ee54c275f452e2
hiroki nagao <honghuizhangwei@gmail.com>
2021-10-22 16:32:48 +0900

    first commit

---
+++ kintaikanri/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('10')->index();
+            $table->varchar('work_id','10')->primary()->index();
+            $table->varchar('name','20')->index();
+            $table->varchar('password','20');
+            $table->int('role')->index()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ kintaikanri/resources/views/monthly_list.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>月別一覧画面</title>
+</head>
+
+<body>
+    <div class='title'>
+        <h1>月別一覧</h1>
+    </div>
+    <div class='title_'>
+        <input type="date" id="date" name="今日の日付" value="" />
+        <button>PDF</button>
+    </div>
+    <ul>
+    <li>日付</li>
+    <li>出勤</li>
+    <li>退勤</li>
+    <li>休憩</li>
+    <li>コメント</li>
+    <li>編集</li>
+    </ul>
+</body>
+
+</html>
