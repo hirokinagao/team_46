cc1435ccf0b2e298747e22ed1a9ac4d5d4ecf230
hiroki nagao <honghuizhangwei@gmail.com>
2021-10-22 17:01:14 +0900

    first commit

--- kintaikanri/database/migrations/2021_10_19_205521_create_work_times.php
+++ kintaikanri/database/migrations/2021_10_19_205521_create_work_times.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKintaikanri extends Migration
+class work_times extends Migration
 {
     /**
      * Run the migrations.
