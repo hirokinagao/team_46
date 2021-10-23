34b9b5a685b859f5dbeb4c78b7df1489ed805b94
Hitomi <nyororo2007@yahoo.co.jp>
2021-10-23 20:33:13 +0900

    ファイルの追加

---
+++ kintaikanri/resources/views/admin.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KintaiKanri</title>
+
+    <!-- BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/side.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+    
+</head>
+
+<body class="home">
+    <div class="container">
+        @include('common.side')
+
+        <div class="main">
+            <div class="main-header">
+                @include('common.header')
+            </div>
+
+            <div class="">
+            </div>
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ kintaikanri/resources/views/common/header.blade.php
@@ -0,0 +1,5 @@
+<div class="header-inner"> 
+    <h2>～画面</h2>
+    <h3>テスト 太郎</h3>
+    <h3>a000000001</h3>
+</div>
