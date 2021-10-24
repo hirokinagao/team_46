680ec66b01aa64d97aa42d438f5e76844aa41097
Hitomi <nyororo2007@yahoo.co.jp>
2021-10-24 13:27:02 +0900

    commit確認

--- kintaikanri/resources/views/admin.blade.php
+++ kintaikanri/resources/views/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>KintaiKanri</title>
+    <title>Kannrisyagaamenn</title>
 
     <!-- BootStrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
@@ -13,6 +13,7 @@
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
     <link href="{{ asset('css/side.css') }}" rel="stylesheet">
     <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+
     
 </head>
 
