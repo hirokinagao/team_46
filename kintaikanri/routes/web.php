34df0df2f0ccdfdeee1ce7665486e1cd1681cfaa
Hitomi <nyororo2007@yahoo.co.jp>
2021-10-25 09:00:59 +0900

    commit

--- kintaikanri/routes/web.php
+++ kintaikanri/routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 /*
+| 全員共通ファイル
+*/
+
+/*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -12,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 
 Route::get('/', function () {
     return view('monthly_list');
