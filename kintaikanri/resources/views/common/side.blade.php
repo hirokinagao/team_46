79061d0528607a62734e428d1f7339ba8d8cb0a4
Hitomi <nyororo2007@yahoo.co.jp>
2021-11-06 14:50:28 +0900

    modify side.blade

--- kintaikanri/resources/views/common/side.blade.php
+++ kintaikanri/resources/views/common/side.blade.php
@@ -4,7 +4,7 @@
 <div class="side">
     <ul class="nav flex-column">
         <li class="nav-item"><img src="./img/logo.png" alt="ロゴ画像"></li>
-        <li class="nav-item"><a href="{{ url('monthly_list.bladeのルート') }}" class="nav-link icon tukibetu">月別一覧</a></li>
+        <li class="nav-item"><a href="{{ url('monthly_list') }}" class="nav-link icon tukibetu">月別一覧</a></li>
         <li class="nav-item"><a href="{{ url('work_time.bladeのルート') }}" class="nav-link icon">勤退登録</a></li>
         @if ( $user_role == 1 )
             <li class="nav-item">
