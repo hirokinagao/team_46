f2851d6976856c08da7013c57808d1a506ec70cc
Hitomi <nyororo2007@yahoo.co.jp>
2021-11-28 14:00:55 +0900

    Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,9 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Schema::defaultStringLength(191);
-        if (\App::environment(['production'])) {
-            \URL::forceScheme('https');
-        }
+        //Schema::defaultStringLength(191);
+        // if (\App::environment(['production'])) {
+        //     \URL::forceScheme('https');
+        // }
     }
 }
